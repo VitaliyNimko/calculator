7bbae6e8c76cdb9d574459c6cd64f4736bdb8f25
vitaliynimko <vitaliynimko@gmail.com>
2019-02-08 13:44:40 +0000

    calculator

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const display = document.querySelector('.calculator input[name=display]');
+
+document.querySelectorAll('.digits button, .opers button')
+    .forEach(btn => btn.addEventListener('click', digitsOperPressed));
+
+function digitsOperPressed(e) {
+    display.value += e.target.innerText;
+}
+
+document.querySelector('.clear')
+    .addEventListener('click', function() {
+        display.value = '';
+    });
+
+document.querySelector('.result')
+    .addEventListener('click', function() {
+        display.value = eval(display.value);
+    })
+
+document.querySelector('.backspace')
+    .addEventListener('click', function() {
+        display.value = display.value.substring(0, display.value.length - 1)
+    });
